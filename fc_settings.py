e3235274fa1f413ba0f0be5ac9c835b426667b6c
benoit <benoit@morus.local>
2024-08-25 14:33:23 +0200

    add GPIO doc

--- fc_settings.py
+++ fc_settings.py
@@ -5,6 +5,16 @@ import logging
 LOG_FILE_PATH = "run/fermentation_controller.log"
 
 
+#GPIOS
+
+# Product Temperature Probe with DS18B20
+# add in /boot/config.txt dtoverlay=w1-gpio,gpiopin=6 for PIN 6
+#
+
+# Chamber temp on GPIO 4
+
+
+
 def init_fc_logger():
     logging.basicConfig(filename=LOG_FILE_PATH,
                         filemode='w',
